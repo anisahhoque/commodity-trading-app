fba9da33e92adea35723dfe6c9baf0f3eebbb694
DCamans <daniel55519@hotmail.co.uk>
2025-04-09 10:13:15 +0100

    Added a trader model with all its attributes, and a folder within views: Views/Trader with an empty cshtml view.

---
+++ CommodityTradingApp/CommodityTradingApp/Models/Trader.cs
@@ -0,0 +1,11 @@
+﻿namespace CommodityTradingApp.Models
+{
+    public class Trader
+    {
+        public int Id { get; set; }
+        public string AccountName { get; set; }
+        public decimal Balance { get; set; }
+        public string UserId { get; set; }
+
+    }
+}
