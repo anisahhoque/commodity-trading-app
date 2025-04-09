82ef25c69f507c3783181cf6ed9c0c54ce35d7e1
anisahhoque <hoqueshabrina@gmail.com>
2025-04-09 15:06:31 +0100

    add httpcontext

---
+++ CommodityTradingApp/CommodityTradingApp/Controllers/CommoditiesController.cs
@@ -0,0 +1,43 @@
+﻿using CommodityTradingApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommodityTradingApp.Controllers
+{
+    public class CommoditiesController : Controller
+
+    {
+        private readonly HttpClient _httpClient;
+        private static List<Commodity> commodities = GetMockCommodities();
+
+        public CommoditiesController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(commodities);
+        }
+        private static List<Commodity> GetMockCommodities()
+        {
+            return new List<Commodity>
+            {
+                new Commodity
+                {
+                    CommodityId = Guid.NewGuid(),
+                    CommodityName = "Gold"
+                },
+                new Commodity
+                {
+                    CommodityId = Guid.NewGuid(),
+                    CommodityName = "Oil"
+
+                },
+                new Commodity
+                {
+                    CommodityId = Guid.NewGuid(),
+                    CommodityName = "Silver"
+
+                }
+            };
+        }
+}
