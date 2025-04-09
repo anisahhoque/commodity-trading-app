37b470e639295590056b5c228a18a883f406b44a
DCamans <daniel55519@hotmail.co.uk>
2025-04-09 10:23:55 +0100

    Created a basic controller with basic values to test the view

---
+++ CommodityTradingApp/CommodityTradingApp/Controllers/TraderController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CommodityTradingApp.Models;
+
+namespace CommodityTradingApp.Controllers
+{
+    public class TraderController : Controller
+    {
+        // GET: Trader/Index
+        // Creating a new instance of the Trader model
+        public IActionResult Index()
+        {
+            Trader trader = new Trader
+            {
+                Id = 1,
+                AccountName = "John Doe",
+                Balance = 10000.00m
+            };
+            return View(trader);
+        }
+    }
+}
