08ff3bbca3b18ddcadeb2e7b91855e97f7884340
anisahhoque <hoqueshabrina@gmail.com>
2025-04-09 11:08:01 +0100

    create model

---
+++ CommodityTradingApp/CommodityTradingApp/Models/Trade.cs
@@ -0,0 +1,20 @@
+﻿namespace CommodityTradingApp.Models
+{
+
+    public class Trade
+    {
+        public Guid TradeID { get; set; }        
+        public Guid TraderID { get; set; }       
+        public Guid CommodityID { get; set; }    
+        public long PricePerUnit { get; set; }   
+        public int Quantity { get; set; }       
+        public bool IsBuy { get; set; }          
+        public DateTime Expiry { get; set; }     
+        public DateTime CreatedAt { get; set; }  
+        public string Bourse { get; set; }       
+        public Guid MitigationID { get; set; }   
+        public bool IsOpen { get; set; }         
+        public string Contract { get; set; }     
+    }
+    
+}
