97d7643ad12413a2911355b14b54d44182c77efe
anisahhoque <hoqueshabrina@gmail.com>
2025-04-10 23:51:04 +0100

    user can log in

---
+++ CommodityTradingApp/CommodityTradingApp/Controllers/LoginController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommodityTradingApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _config;
+        private readonly string _apiUrl;
+        public LoginController(IConfiguration config, HttpClient httpClient)
+        {
+            _config = config;
+            _httpClient = httpClient;
+            _apiUrl = _config["api"] + "Authentication/";
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            var loginData = new
+            {
+                Username = username,
+                Password = password
+            };
+
+            
+            var jsonContent = new StringContent(
+                System.Text.Json.JsonSerializer.Serialize(loginData),
+                System.Text.Encoding.UTF8,
+                "application/json"
+            );
+
+            var response = await _httpClient.PostAsync(_apiUrl + "Login", jsonContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Error = "Login failed.";
+                return View();
+            }
+        }
+    }
+}
