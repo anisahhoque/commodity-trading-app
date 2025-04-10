d4917b5433d3a54389bb9ff5c16e839ba1209422
DCamans <daniel55519@hotmail.co.uk>
2025-04-10 10:22:38 +0100

    Implemented a complete TraderDetailsViewModel

--- CommodityTradingApp/CommodityTradingApp/Models/TraderDetailsViewModel.cs
+++ CommodityTradingApp/CommodityTradingApp/Models/TraderDetailsViewModel.cs
@@ -2,5 +2,9 @@
 {
     public class TraderDetailsViewModel
     {
+        public Trader Trader { get; set; }
+        public List<Trade> ActiveTrades { get; set; }
+        public List<Trade> HistoricalTrades { get; set; }
+
     }
 }
