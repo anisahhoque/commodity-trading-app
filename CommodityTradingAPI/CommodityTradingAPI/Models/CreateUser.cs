32c8d2230b88ee5e3a2e504b2df55abc9a5dec1e
Candice-Bennett <Candice.Bennett123@gmail.com>
2025-04-10 10:58:22 +0100

    Created a basic controller for the user side of API.

---
+++ CommodityTradingAPI/CommodityTradingAPI/Models/CreateUser.cs
@@ -0,0 +1,13 @@
+﻿namespace CommodityTradingAPI.Models
+{
+    public class CreateUser
+    {
+
+        public string Username { get; set; } = null!;
+
+        public string PasswordRaw { get; set; } = null!;
+
+        public string Country { get; set; }
+
+    }
+}
