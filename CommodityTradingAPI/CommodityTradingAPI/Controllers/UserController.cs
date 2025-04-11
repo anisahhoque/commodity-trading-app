4ac7249bc167f01fbf24dfcedd5f35c3c4a52417
anisahhoque <hoqueshabrina@gmail.com>
2025-04-11 15:40:15 +0100

    broken

--- CommodityTradingAPI/CommodityTradingAPI/Controllers/UserController.cs
+++ CommodityTradingAPI/CommodityTradingAPI/Controllers/UserController.cs
@@ -5,11 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using CommodityTradingAPI.Models;
-<<<<<<< HEAD
-using CommodityTradingAPI.Services;
-=======
 using ILogger = CommodityTradingAPI.Services.ILogger;
->>>>>>> 252cbd334b18a368d8813137f79b38c8a52b47e3
 
 namespace CommodityTradingAPI.Controllers
 {
@@ -18,20 +14,12 @@ namespace CommodityTradingAPI.Controllers
     public class UserController : ControllerBase
     {
         private readonly CommoditiesDbContext _context;
-<<<<<<< HEAD
-        private readonly AuditLogService _auditLogService;
-        public UserController(CommoditiesDbContext context, AuditLogService auditLogService)
-        {
-            _context = context;
-            _auditLogService = auditLogService;
-=======
         private ILogger _auditLogService;
 
         public UserController(CommoditiesDbContext context, ILogger logger)
         {
             _context = context;
             _auditLogService = logger;
->>>>>>> 252cbd334b18a368d8813137f79b38c8a52b47e3
         }
 
         [HttpGet]
