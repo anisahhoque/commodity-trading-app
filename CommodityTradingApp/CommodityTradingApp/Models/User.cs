bc057a464204ae0fd413477ccd38058722eba436
DCamans <daniel55519@hotmail.co.uk>
2025-04-11 09:49:14 +0100

    Added a User Model

---
+++ CommodityTradingApp/CommodityTradingApp/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommodityTradingApp.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }
+        public int CountryID { get; set; }
+    }
+}
+
