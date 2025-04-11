982953e9b9177d2c398917cff42aa595d3787373
DCamans <daniel55519@hotmail.co.uk>
2025-04-11 14:35:19 +0100

    Implemented country controller

---
+++ CommodityTradingAPI/CommodityTradingAPI/Controllers/CountryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CommodityTradingAPI.Data;
+
+namespace CommodityTradingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class CountryController : ControllerBase
+    {
+        private readonly CommoditiesDbContext _context;
+
+        public CountryController(CommoditiesDbContext context)
+        {
+            _context = context;
+        }
+        //GET: api/country
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var countries = _context.Countries.ToList();
+
+            return Ok(countries);
+        }
+    }
+}
