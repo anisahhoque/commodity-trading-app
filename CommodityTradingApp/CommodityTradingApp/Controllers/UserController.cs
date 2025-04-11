be6b81c53e0b7779818722546bd4521de147ddb2
DCamans <daniel55519@hotmail.co.uk>
2025-04-11 09:50:44 +0100

    Implemented User/Details/{guid} endpoint

--- CommodityTradingApp/CommodityTradingApp/Controllers/UserController.cs
+++ CommodityTradingApp/CommodityTradingApp/Controllers/UserController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CommodityTradingApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CommodityTradingApp.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Index()
+        // GET: User/Details/{guid}
+        public IActionResult Details(Guid id)
         {
-            return View();
+            // Simulate fetching user data (replace with DB call)
+            var users = new List<User>
+            {
+                new User { Id = Guid.Parse("e7db167e-d1b3-4b7e-b417-72a6cb85943c"), Username = "JDM", PasswordHash = "sdljfalsdjfklasdjlkaj234daljf" },
+                new User { Id = Guid.Parse("1d6ffbd2-5c44-4a1e-93b2-0fc690f6e2b9"), Username = "Admin", PasswordHash = "asdfdskfj3lk4j5lkj234lkj" }
+            };
+
+            var user = users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+                return NotFound();
+
+            return View(user);
         }
     }
 }
--- CommodityTradingApp/CommodityTradingApp/Models/User.cs
+++ CommodityTradingApp/CommodityTradingApp/Models/User.cs
@@ -5,7 +5,7 @@ namespace CommodityTradingApp.Models
     public class User
     {
         public Guid Id { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string PasswordHash { get; set; }
         public int CountryID { get; set; }
     }
