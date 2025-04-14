9c5637ab3bd2d21b5dc0ab5ce34048cab9c218ed
anisahhoque <hoqueshabrina@gmail.com>
2025-04-14 17:39:41 +0100

    values show on trade now page

---
+++ CommodityTradingApp/CommodityTradingApp/Models/CommodityWithPriceViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CommodityTradingApp.Models
+{
+    public class CommodityWithPriceViewModel
+    {
+        public Commodity Commodity { get; set; }
+
+        public decimal Price { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public decimal AbsoluteChange { get; set; }
+        public decimal RelativeChange { get; set; }
+    }
+
+}
