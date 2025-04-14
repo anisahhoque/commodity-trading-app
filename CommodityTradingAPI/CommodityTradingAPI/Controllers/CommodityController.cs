67ca40c2dedbc79f8d0fbc867dad5f01de16287d
matt-severnsl <trainee.matt.severn@sigmalabs.co.uk>
2025-04-14 10:37:58 +0100

    error fix

--- CommodityTradingAPI/CommodityTradingAPI/Controllers/CommodityController.cs
+++ CommodityTradingAPI/CommodityTradingAPI/Controllers/CommodityController.cs
@@ -16,7 +16,7 @@ namespace CommodityTradingAPI.Controllers
         private static readonly List<string> SupportedCommodities = new List<string>
         {
             "gold"
-            //, "platinum", "lean_hogs", "oat", "aluminium", "soybean_meal", "lumber", "micro_gold", "feeder_cattle", "rough_rice", "palladium"
+            , "platinum", "lean_hogs", "oat", "aluminium", "soybean_meal", "lumber", "micro_gold", "feeder_cattle", "rough_rice", "palladium"
         };
 
         public CommodityController(IHttpClientFactory httpClientFactory, IConfiguration configuration)
