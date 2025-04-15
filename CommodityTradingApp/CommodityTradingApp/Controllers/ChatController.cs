7c0af68a525f491b4be2d228e49cc51f3531606e
anisahhoque <hoqueshabrina@gmail.com>
2025-04-15 13:05:05 +0100

    remove checks

--- CommodityTradingApp/CommodityTradingApp/Controllers/ChatController.cs
+++ CommodityTradingApp/CommodityTradingApp/Controllers/ChatController.cs
@@ -8,8 +8,6 @@ namespace CommodityTradingApp.Controllers
         [Authorize]
         public async Task<IActionResult> Index()
         {
-            var token = HttpContext.Session.GetString("RocketChatToken");
-            var userId = HttpContext.Session.GetString("RocketChatUserId");
             return View();
         }
     }
